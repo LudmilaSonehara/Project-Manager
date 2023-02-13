b8e629c184fda78ea548659d7eada12ae21a0505
Ludmila Sonehara <ludmilasonehara.dev@gmail.com>
2023-02-13 18:43:19 -0300

    init

---
+++ src/components/pages/Company.js
@@ -0,0 +1,17 @@
+import styles from './Company.module.css'
+
+function Company() {
+    return (
+        <section className={styles.Container}>
+            <h1>Sobre a empresa</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam varius felis eu turpis tincidunt tempor vel a nunc. Etiam facilisis tristique mattis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec justo sed ligula efficitur dapibus ac non nibh. Nam pharetra non est in congue. Fusce hendrerit, libero nec porttitor cursus, dui odio rhoncus elit, eu malesuada augue neque eu enim. Aenean elit lectus, blandit aliquet justo at, maximus porta erat. Donec tristique, dolor quis tempor faucibus, turpis ligula iaculis enim, at ultricies odio purus eu enim. Quisque luctus mollis felis.
+            </p>
+            <p>
+                Donec faucibus purus eget purus semper, ut eleifend dui finibus. Nunc tincidunt tortor non odio scelerisque, sit amet fermentum tortor porttitor. Donec pellentesque metus non egestas ornare. In condimentum maximus augue quis aliquet. Curabitur vestibulum turpis ut cursus convallis. Integer auctor dui id tellus commodo tristique. Ut dignissim magna ac suscipit aliquam. Praesent eu libero nisl. Fusce porta maximus lacus ac consequat. Nulla sit amet justo hendrerit, eleifend purus et, luctus erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar fermentum ex ut dignissim. Sed dictum vel tellus sit amet eleifend. Sed quis ante diam. Nullam ante lacus, sollicitudin id sodales sed, venenatis ut justo.
+            </p>
+        </section>
+    )
+}
+
+export default Company
---
+++ src/components/pages/Contact.js
@@ -0,0 +1,12 @@
+import styles from './Contact.module.css'
+
+function Contact() {
+    return (
+        <section className={styles.Container}>
+            <h1>Contact</h1>
+            <div className="elfsight-app-f66ddfc6-5f16-491c-8acc-3c408f0fa30e" />
+        </section>
+    )
+}
+
+export default Contact;
